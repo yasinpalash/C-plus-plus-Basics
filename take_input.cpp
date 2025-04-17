5b876d2d70fa14374d8922b86a5193e8885d0d4a
yasin <yasinmiapolash@gmail.com>
2025-04-17 23:27:59 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ take_input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x, y;
+
+    cin >> x >> y;
+
+    cout << "the value of x is " << x << endl;
+    cout << "the value of y is " << y << endl;
+    return 0;
+}
