7fa823a07c5ae9ec8e61fb5f2291b2c368e0e75a
yasin <yasinmiapolash@gmail.com>
2025-04-17 23:18:23 +0600

    Refactor output statements in hey.cpp for consistency

--- hey.cpp
+++ hey.cpp
@@ -1,10 +1,11 @@
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
 
-    std::cout << "Hey there!";
-
+    cout << "Hey there!" << "\n";
+    cout << "This is Yasin!";
     return 0;
 }
