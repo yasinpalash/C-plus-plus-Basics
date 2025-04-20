55bfaddf4dc5facfe04490ee41caf6ab9bf34d3c
yasin <yasinmiapolash@gmail.com>
2025-04-20 22:06:04 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ data_type.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int
+    // int a = 5;
+    // long
+    // long b = 10;
+    // cin >> b;
+    // long long
+    // long long g = 15000000000;
+
+    // float, double
+
+    // float c = 5.5;
+    // float d = 10.5;
+
+    // cout << "value of a is : " << c;
+
+    // string and getline
+
+    string s1;
+    string s2;
+
+    cin >> s1;
+    cin >> s2;
+    cout << "value of s1 is : " << s1 << endl;
+    cout << "value of s2 is : " << s2 << endl;
+
+    return 0;
+}
