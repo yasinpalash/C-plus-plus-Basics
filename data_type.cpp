c17bfba44d0d93f7635530ee148c071c3756e31d
yasin <yasinmiapolash@gmail.com>
2025-04-20 22:14:18 +0600

    Refactor data_type.cpp to uncomment string input handling and add output for character variable Add c_cpp_properties.json for Visual Studio Code configuration

--- data_type.cpp
+++ data_type.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
@@ -20,13 +21,25 @@ int main()
 
     // string and getline
 
-    string s1;
-    string s2;
+    // string s1;
+    // string s2;
 
-    cin >> s1;
-    cin >> s2;
-    cout << "value of s1 is : " << s1 << endl;
-    cout << "value of s2 is : " << s2 << endl;
+    // cin >> s1;
+    // cin >> s2;
+    // cout << "value of s1 is : " << s1 << endl;
+    // cout << "value of s2 is : " << s2 << endl;
+
+    // getline
+    // intput string with spaces
+    // string str;
+
+    // getline(cin, str);
+
+    // cout << "value of str is : " << str << endl;
+
+    // char
+    char ch = 'a';
+    cout << "value of ch is : " << ch << endl;
 
     return 0;
 }
