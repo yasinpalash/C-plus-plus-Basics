59098d2e1fc36255f412b3c017d2fa05b983de09
yasin <yasinmiapolash@gmail.com>
2025-04-21 21:43:19 +0600

    implement the number event or odd

--- if_else/event_or_odd.cpp
+++ if_else/event_or_odd.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-
+    cout << "Input a int number: ";
     int num;
+
+    cin >> num;
+
+    if (num / 2 == 0)
+
+    {
+        cout << "this is event number";
+    }
+    else
+    {
+        cout << "This is odd number ";
+    }
 }
