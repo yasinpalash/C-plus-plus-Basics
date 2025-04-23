798ad6210e62742766caee74fa40dc9d63612926
yasin <yasinmiapolash@gmail.com>
2025-04-23 22:05:22 +0600

    apply good approach

---
+++ if_else/best_approch_for_grading_system.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int marks = 45;
+    char grade;
+
+    if (marks < 25)
+    {
+        grade = 'F';
+    }
+    else if (marks <= 44)
+    {
+        grade = 'E';
+    }
+    else if (marks <= 49)
+    {
+        grade = 'D';
+    }
+    else if (marks <= 59)
+    {
+        grade = 'C';
+    }
+    else if (marks <= 69)
+    {
+        grade = 'B';
+    }
+    else if (marks >= 70)
+    {
+        grade = 'A';
+    }
+    else
+    {
+
+        grade = 'X';
+    }
+
+    cout << "Grade : " << grade << endl;
+
+    return 0;
+}
