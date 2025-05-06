b63721c1a8fca827fddf7033fbe8593bd7838b68
yasin <yasinmiapolash@gmail.com>
2025-05-06 23:31:59 +0600

    nested switch case

---
+++ switch_case/Nested_switch_statements.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(
+
+)
+{
+    int x = 2;
+    int y = 3;
+
+    switch (x)
+    {
+
+    case 1:
+        cout << "x is 1." << endl;
+        switch (y)
+        {
+        case 1:
+            cout << "y is 1." << endl;
+            break;
+
+        default:
+            cout << "y is not  1." << endl;
+        }
+
+    default:
+        cout << "x  is not  1." << endl;
+    }
+}
