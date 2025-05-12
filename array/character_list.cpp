08432615937a0f56d09b534c2c2649a0d23a734a
yasin <yasinmiapolash@gmail.com>
2025-05-12 23:33:42 +0600

    string array

---
+++ array/character_list.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string name = "Yasin";
+
+    for (int i = 0; i < name.length(); i++)
+    {
+
+        cout << name[i] << endl;
+    }
+    return 0;
+}
